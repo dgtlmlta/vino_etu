80a46924fa4d082e18488a8d680bf07e5605a6e5
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-23 09:48:41 -0500

    Retirer des commentaires

--- resources/frontend/src/app/pages/creation-bouteille/creation-bouteille.component.ts
+++ resources/frontend/src/app/pages/creation-bouteille/creation-bouteille.component.ts
@@ -104,7 +104,7 @@ export class CreationBouteilleComponent implements OnInit {
               this.snackbar.open('Vous avez ajouté une bouteille personnalisé au catalogue', 'Fermer')
 
               this.router.navigate(['/bouteilles']);
-          }); 
+          });
   }
 
   /**
@@ -118,31 +118,4 @@ export class CreationBouteilleComponent implements OnInit {
       comparerCellierId(id1: number, id2: string|number): boolean {
       return id1 == id2;
   }
-
- /* uploadFileEvt(imgFile: any) {
-    if (imgFile.target.files && imgFile.target.files[0]) {
-      this.fileAttr = '';
-      Array.from(imgFile.target.files).forEach((file: any) => {
-        this.fileAttr += file.name + ' - ';
-      });
-
-      // HTML5 FileReader API
-      let reader = new FileReader();
-      reader.onload = (e: any) => {
-        let image = new Image();
-        image.src = e.target.result;
-        image.onload = rs => {
-          let imgBase64Path = e.target.result;
-        };
-      };
-      reader.readAsDataURL(imgFile.target.files[0]);
-      
-      // Reset if duplicate image uploaded again
-      this.fileInput.nativeElement.value = "";
-    } else {
-      this.fileAttr = 'Fichier';
-    }
-  } */
-
-
 }
