2afce016161dd447470074b13f122ca764184f23
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-24 16:55:52 -0500

    Création d'un seeder pour la liste d'achat

--- resources/frontend/src/app/services/bouteille-de-vin.service.ts
+++ resources/frontend/src/app/services/bouteille-de-vin.service.ts
@@ -13,9 +13,9 @@ import { map } from 'rxjs/operators';
 })
 export class BouteilleDeVinService {
 
-    private url:string = "http://127.0.0.1:8000/api";
+    // private url:string = "http://127.0.0.1:8000/api";
     // private url: string = "http://kalimotxo-vino.akira.dev/api";
-    // private url: string = new URL(window.location.href).origin + "/api";
+    private url: string = new URL(window.location.href).origin + "/api";
 
 
     constructor(private servAuth: AuthService, private http: HttpClient, private dialog: MatDialog) {
