85065d259eba29f88663cef65fe07ed3e29a3c1d
Nicolas Rouleau <nicolas@digitalmilitia.net>
2021-11-24 16:56:33 -0500

    Création d'un seeder pour la liste d'achat

--- database/seeders/ListeAchatSeeder.php
+++ database/seeders/ListeAchatSeeder.php
@@ -2,17 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\ListeAchat;
 use Illuminate\Database\Seeder;
 
-class ListeAchatSeeder extends Seeder
-{
+class ListeAchatSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        $listeAchat = [
+            "nom"         => "Mon premier cellier",
+            "description" => "Mon premier cellier avec Vino",
+            "users_id"    => 1,
+        ];
+
+        ListeAchat::create($listeAchat);
     }
 }
